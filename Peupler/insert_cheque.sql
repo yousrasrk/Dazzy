ccef8e59234a3fdd47d1fa6eb3984d9fec1e5dbd
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-05-31 02:50:53 +0100

    Add files via upload

---
+++ Peupler/insert_cheque.sql
@@ -0,0 +1,10 @@
+insert into cheque values ('C1' , 'T1' , TO_DATE('20190701','YYYYMMDD') , 2000.32);
+insert into cheque values ('C2' , 'T1' , TO_DATE('20190801','YYYYMMDD') , 22010.32);
+insert into cheque values ('C3' , 'T1' , TO_DATE('20191001','YYYYMMDD') , 2102.32);
+insert into cheque values ('C4' , 'T2' , TO_DATE('20190701','YYYYMMDD') , 10400);
+insert into cheque values ('C5' , 'T2' , TO_DATE('20190813','YYYYMMDD') , 3000.32);
+insert into cheque values ('C6' , 'T10' ,TO_DATE('20190701','YYYYMMDD') , 90000.32);
+insert into cheque values ('C7' , 'T9' , TO_DATE('20190713','YYYYMMDD') , 3200.32);
+insert into cheque values ('C8' , 'T3' , TO_DATE('20190808','YYYYMMDD') , 5030.32);
+insert into cheque values ('C9' , 'T3' , TO_DATE('20190901','YYYYMMDD') , 50600.32);
+insert into cheque values ('C10' , 'T3' , TO_DATE('20190910','YYYYMMDD') , 90000.32);
